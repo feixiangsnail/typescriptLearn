08046f748267e06d0a81a5183e719fcf11c365a5
feixiangsnail <feixiangsnail@hotmail.com>
2018-04-17 17:53:30 +0800

    egretpack.js

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,6 @@
 
 //import MyGrid from './MyGrid'
+
 class Main extends egret.DisplayObjectContainer{
     public constructor(){
         super();
@@ -9,6 +10,13 @@ class Main extends egret.DisplayObjectContainer{
         // var _myGrid:MyGrid = new MyGrid();
         // this.addChild( _myGrid );
         console.log('onaddtostage')
+        var shp:egret.Shape = new egret.Shape();
+        shp.graphics.beginFill( 0x00ff00 );
+        shp.graphics.drawRect( 0, 0, 100, 100 );
+        shp.graphics.endFill();
+        shp.x = 100;
+        shp.y = 100;
+        this.addChild( shp );
     }
 }
 egret.registerClass(Main, "Main");
