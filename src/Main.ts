bcd9e4b560b69a6a3cbc57aad26db2405f74ce04
feixiangsnail <feixiangsnail@hotmail.com>
2018-04-17 18:12:42 +0800

    finally

--- src/Main.ts
+++ src/Main.ts
@@ -6,18 +6,52 @@ class Main extends egret.DisplayObjectContainer{
         super();
         this.addEventListener(egret.Event.ADDED_TO_STAGE,this.onAddToStage,this);
     }
-    private onAddToStage(event:egret.Event){
-        // var _myGrid:MyGrid = new MyGrid();
-        // this.addChild( _myGrid );
-        console.log('onaddtostage')
+    // private onAddToStage(event:egret.Event){
+    //     var shp:egret.Shape = new egret.Shape();
+    //     shp.graphics.beginFill( 0xff0000 );
+    //     shp.graphics.drawRect( 0,0,100,100);
+    //     shp.graphics.endFill();
+    //     this.addChild( shp );
+    //     var shp2:egret.Shape = new egret.Shape();
+    //     shp2.graphics.beginFill( 0x00ff00 );
+    //     shp2.graphics.drawCircle( 0,0, 20);
+    //     shp2.graphics.endFill();
+    //     this.addChild( shp2 );
+    //     shp2.x = 20;
+    //     shp2.y = 20;
+    // }
+
+
+    private onAddToStage(event:egret.Event)
+    {
+        this.drawText();
         var shp:egret.Shape = new egret.Shape();
-        shp.graphics.beginFill( 0x00ff00 );
-        shp.graphics.drawRect( 0, 0, 100, 100 );
+        shp.graphics.beginFill( 0xff0000 );
+        shp.graphics.drawRect( 0,0,100,100);
         shp.graphics.endFill();
-        shp.x = 100;
-        shp.y = 100;
+        shp.width = 100;
+        shp.height = 100;
         this.addChild( shp );
+        var isHit:boolean = shp.hitTestPoint( 10, 10 );
+        this.infoText.text = "isHit: " + isHit;
     }
+    private infoText:egret.TextField;
+    private drawText()
+    {
+        this.infoText = new egret.TextField();
+        this.infoText.y = 200;
+        this.infoText.text = "isHit";
+        this.addChild( this.infoText );
+    }
+
+
+
+
+
+
+
+
+
 }
 egret.registerClass(Main, "Main");
 window["Main"] = Main;
