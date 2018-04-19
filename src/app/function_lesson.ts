a958dabfcf4bd1cfd46e2a3ae5b81a38e5bcb26a
feixiangsnail <feixiangsnail@hotmail.com>
2018-04-19 14:27:59 +0800

    helloworld

--- src/app/function_lesson.ts
+++ src/app/function_lesson.ts
@@ -2,7 +2,7 @@ function add(x:number,y:number):number{
     return x+y;
 }
 console.log(add(3,6));
-
+console.log('helloworld')
 let myAdd: (x: number, y: number) => number =
     function(x: number, y: number): number { return x + y; };
     console.log(myAdd(9,2));
